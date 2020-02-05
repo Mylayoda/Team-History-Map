0c40d4cdccfc3c4af0c0a290335a7a6c17933ce1
Derrick Yirenkyi <21703340@bucks.ac.uk>
2020-02-05 11:21:30 +0000

    Add project files.

---
+++ IdentityCustomisationTest/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityCustomisationTest.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string CustomTag { get; set; }
+        //public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ IdentityCustomisationTest/Data/Migrations/20181210174856_AddedNewUserFields.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityCustomisationTest.Data.Migrations
+{
+    public partial class AddedNewUserFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomTag",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomTag",
+                table: "AspNetUsers");
+        }
+    }
+}
